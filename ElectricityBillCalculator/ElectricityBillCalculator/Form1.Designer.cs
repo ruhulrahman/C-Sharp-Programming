9595e4476858926a4af7dd983e401d4950b256a2
ruhulrahman <ruhulrahman2233@gmail.com>
2019-09-08 23:08:51 +0600

    Electricity Bill Calculator Added

---
+++ ElectricityBillCalculator/ElectricityBillCalculator/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ElectricityBillCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonResident = new System.Windows.Forms.RadioButton();
+            this.radioButtonIndustrial = new System.Windows.Forms.RadioButton();
+            this.radioButtonCommercial = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.peakHourBox = new System.Windows.Forms.TextBox();
+            this.offPeakHourBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox1.Controls.Add(this.radioButtonCommercial);
+            this.groupBox1.Controls.Add(this.radioButtonIndustrial);
+            this.groupBox1.Controls.Add(this.radioButtonResident);
+            this.groupBox1.Location = new System.Drawing.Point(28, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Type";
+            // 
+            // radioButtonResident
+            // 
+            this.radioButtonResident.AutoSize = true;
+            this.radioButtonResident.Location = new System.Drawing.Point(24, 31);
+            this.radioButtonResident.Name = "radioButtonResident";
+            this.radioButtonResident.Size = new System.Drawing.Size(77, 17);
+            this.radioButtonResident.TabIndex = 0;
+            this.radioButtonResident.TabStop = true;
+            this.radioButtonResident.Text = "Residential";
+            this.radioButtonResident.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonIndustrial
+            // 
+            this.radioButtonIndustrial.AutoSize = true;
+            this.radioButtonIndustrial.Location = new System.Drawing.Point(24, 103);
+            this.radioButtonIndustrial.Name = "radioButtonIndustrial";
+            this.radioButtonIndustrial.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonIndustrial.TabIndex = 1;
+            this.radioButtonIndustrial.TabStop = true;
+            this.radioButtonIndustrial.Text = "Industrial";
+            this.radioButtonIndustrial.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCommercial
+            // 
+            this.radioButtonCommercial.AutoSize = true;
+            this.radioButtonCommercial.Location = new System.Drawing.Point(24, 68);
+            this.radioButtonCommercial.Name = "radioButtonCommercial";
+            this.radioButtonCommercial.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonCommercial.TabIndex = 2;
+            this.radioButtonCommercial.TabStop = true;
+            this.radioButtonCommercial.Text = "Commercial";
+            this.radioButtonCommercial.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.offPeakHourBox);
+            this.groupBox2.Controls.Add(this.peakHourBox);
+            this.groupBox2.Location = new System.Drawing.Point(254, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(196, 179);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "KWH Used";
+            // 
+            // peakHourBox
+            // 
+            this.peakHourBox.Location = new System.Drawing.Point(90, 46);
+            this.peakHourBox.Name = "peakHourBox";
+            this.peakHourBox.Size = new System.Drawing.Size(100, 20);
+            this.peakHourBox.TabIndex = 0;
+            // 
+            // offPeakHourBox
+            // 
+            this.offPeakHourBox.Location = new System.Drawing.Point(90, 91);
+            this.offPeakHourBox.Name = "offPeakHourBox";
+            this.offPeakHourBox.Size = new System.Drawing.Size(100, 20);
+            this.offPeakHourBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Peak Hour";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Off Peak Hour";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 234);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total Bill";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Location = new System.Drawing.Point(78, 231);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(146, 20);
+            this.resultBox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(254, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Calculate Bill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(744, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "Electricity Bill Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonCommercial;
+        private System.Windows.Forms.RadioButton radioButtonIndustrial;
+        private System.Windows.Forms.RadioButton radioButtonResident;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox offPeakHourBox;
+        private System.Windows.Forms.TextBox peakHourBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ElectricityBillCalculator/ElectricityBillCalculator/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElectricityBillCalculator
+{
+    public partial class Form1 : Form
+    {
+        private Boolean residential, commercial, industrial;
+        private float peakHour, offPeakHour, result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            input();
+
+            if(radioButtonResident.Checked == true)
+            {
+                result = peakHour * 10;
+                output(result);
+            }else if(radioButtonCommercial.Checked == true)
+            {
+                if (peakHour <= 800)
+                {
+                    result = peakHour * 50;
+                    output(result);
+                }
+                else
+                {
+                    result = peakHour * 50;
+                    float extraHour = peakHour - 800;
+                    double extraBill = extraHour * 70.042;
+                    result = result + Convert.ToSingle(extraBill);
+                    output(result);
+                }
+                
+            }else if(radioButtonIndustrial.Checked == true)
+            {
+                float peakTotalBill, offPeakTotalBill;
+
+                if (peakHour <= 800)
+                {
+                    peakTotalBill = peakHour * 70;
+                    output(result);
+                }
+                else
+                {
+                    result = peakHour * 70;
+                    float extraHour = peakHour - 800;
+                    double extraBill = extraHour * 70.062;
+                    peakTotalBill = result + Convert.ToSingle(extraBill);
+                }
+
+                if (offPeakHour <= 800)
+                {
+                    offPeakTotalBill = offPeakHour * 35;
+                }
+                else
+                {
+                    result = peakHour * 35;
+                    float extraHour = peakHour - 800;
+                    double extraBill = extraHour * 35.025;
+                    offPeakTotalBill = result + Convert.ToSingle(extraBill);
+                }
+
+                result = peakTotalBill + offPeakTotalBill;
+
+                output(result);
+
+            }
+        }
+
+        private void input()
+        {
+            peakHour = float.Parse(peakHourBox.Text);
+            offPeakHour = float.Parse(offPeakHourBox.Text);
+        }
+        private void output(float result)
+        {
+            resultBox.Text = result.ToString();
+        }
+    }
+}
