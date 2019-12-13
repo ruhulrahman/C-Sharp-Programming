a4dda86204a53334b94ff5d0d09c7996ada12f67
ruhulrahman <ruhulrahman2233@gmail.com>
2019-12-13 15:55:28 +0600

    change

--- Store Mangement/AddProductForm.cs
+++ Store Mangement/AddProductForm.cs
@@ -6,19 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Store_Mangement
 {
     public partial class AddProductForm : Form
     {
+        enum Category
+        {
+            Electronics = 1,
+            Computers = 2,
+            Printers = 3,
+            Wires = 4,
+        }
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
         public AddProductForm()
         {
             InitializeComponent();
+            con.ConnectionString = "Data Source=LAB_612_17;Initial Catalog=Storemanagement;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             textBoxTotalPrice.Text = 0.ToString();
+            foreach (var item in Enum.GetValues(typeof(Category)))
+            {
+                comboBoxProductCat.Items.Add(item);
+            }
         }
         private void button5_Click(object sender, EventArgs e)
         {
+            int i = 0;
+            con.Open();
+            com.Connection = con;
+            //com.CommandText = "Select * from user_info";
+            //SqlDataReader dr = com.ExecuteReader();
             string productName = textBoxProductName.Text;
             string productModel = textBoxProductModel.Text;
             string prodExpireDate = dateTimePickerProdExipire.Text;
@@ -29,14 +49,16 @@ namespace Store_Mangement
 
             if (productName == "" || productModel == "" || prodExpireDate == "" || productCat == "" || textBoxTotalPrice.Text == "")
             {
-                MessageBox.Show("Please fill these fields");
+                MessageBox.Show("Please fill the filds");
             }
             else
-            {
-                int totalPrice = productPrice * productQuantity;
-                textBoxTotalPrice.Text = totalPrice.ToString();
-            }
+            {              
 
+                com.CommandText = "@INSERT INTO products (userId, name, userName, email, phone, password) VALUES ('" + productName + "','" + productModel + "','" + prodExpireDate + "','" + productCat + "','" + productPrice + "','" + productQuantity + "')";
+                con.Close();
+                MessageBox.Show("Producted added successfully");
+            }
+            
         }
 
         public void Multiply()
--- Store Mangement/AddUserForm.Designer.cs
+++ Store Mangement/AddUserForm.Designer.cs
@@ -74,7 +74,7 @@
             this.btnInsertUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnInsertUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnInsertUser.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnInsertUser.Location = new System.Drawing.Point(245, 253);
+            this.btnInsertUser.Location = new System.Drawing.Point(245, 276);
             this.btnInsertUser.Name = "btnInsertUser";
             this.btnInsertUser.Size = new System.Drawing.Size(271, 42);
             this.btnInsertUser.TabIndex = 38;
